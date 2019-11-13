c4c4604a26222bd788e59dd9a240577159c59907
selensr <ksmmevzuat@gmail.com>
2019-11-13 15:08:46 +0300

    completed tasks

---
+++ 2.js
@@ -0,0 +1,3 @@
+var numbers = [1, 4, 9];
+
+var triples = numbers.map(num => num * 3);
---
+++ 3.js
@@ -0,0 +1,5 @@
+var numbers = [12, 5, 20, 130, 44, 9, 240];
+
+var arr = numbers.filter(num => num < 15);
+
+arr;
